b7a92195ec2ebd31e95cde5cd2e2594d830e6f7e
VANKE\c-xuyh12 <c-xuyh12@vanke.com>
2024-07-18 19:21:16 +0800

    T36: 着色器代码整理

--- hw3d/PhongPSSpecNormalMap.hlsl
+++ hw3d/PhongPSSpecNormalMap.hlsl
@@ -1,6 +1,6 @@
 cbuffer LightCBuf
 {
-    float3 lightPos; // 点光源位置
+    float3 viewLightPos; // 点光源位置
     float3 ambient; // 环境光颜色
     float3 diffuseColor; // 漫反射颜色
     float diffuseIntensity; // 漫反射强度
@@ -25,42 +25,62 @@ Texture2D normalMap; // 法线贴图
 
 SamplerState splr; // 采样器
 
-float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 tangent : Tangent, float3 bitangent : Bitangent, float2 tc : Texcoord) : SV_TARGET
+// 将法线方向乘上切线空间的矩阵
+float3 MapNormalViewSpace(const float3 tangent, const float3 bitangent, const float3 normal, const float2 tc, Texture2D normalMap, SamplerState splr)
 {
+    const float3x3 tanToTarget = float3x3(tangent, bitangent, normal);
+        // unpack normal data
+    const float3 normalSample = normalMap.Sample(splr, tc).xyz;
+    const float3 tanNormal = normalSample * 2.0f - 1.0f;
+    
+    return normalize(mul(tanNormal, tanToTarget));
+}
+
+// 点光源光强衰减
+float Attenuate(uniform float attConst, uniform float attLin, uniform float attQuad, const in float distFragToLight)
+{
+    return 1 / (attQuad * (distFragToLight * distFragToLight) + attLin * distFragToLight + attConst);
+}
+
+// 漫反射颜色
+float3 Diffuse(uniform float3 diffuseColor, // 漫反射颜色  
+                uniform float diffuseIntensity, // 漫反射强度
+                const in float att, // 光强衰减
+                const in float3 viewDirFragToLight, // 模型顶点到点光源的方向
+                const in float3 viewNormal) // 模型顶点的法线方向
+{
+    return diffuseColor * diffuseIntensity * att * max(0.0f, dot(viewDirFragToLight, viewNormal));
+}
+
+// 高光计算
+float3 Speculate(const in float3 specularColor,
+                    uniform float specularIntensity,
+                    const in float3 viewNormal,
+                    const in float3 viewFragToLight,
+                    const in float3 viewPos,
+                    const in float att,
+                    const in float specularPower)
+{
+    	// 镜面高光计算
+    const float3 w = viewNormal * dot(viewFragToLight, viewNormal);
+    const float3 r = 2.0f * w - viewFragToLight; // 反射光R的计算
+    
+    return att * (diffuseColor * diffuseIntensity) * pow(max(0.0f, dot(normalize(-r), normalize(viewPos))), specularPower);
+}
+
+float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 viewTangent : Tangent, float3 viewBitangent : Bitangent, float2 tc : Texcoord) : SV_TARGET
+{
+    viewNormal = normalize(viewNormal);
     if (normalMapEnabled)
     {
-        // build the tranform (rotation) into tangent space
-        const float3x3 tanToView = float3x3(
-            normalize(tangent),
-            normalize(bitangent),
-            normalize(viewNormal)
-        );
-        // unpack normal data
-        const float3 normalSample = normalMap.Sample(splr, tc).xyz;
-        viewNormal.x = normalSample.x * 2.0f - 1.0f;
-        viewNormal.y = -normalSample.y * 2.0f + 1.0f;
-        viewNormal.z = normalSample.z;
-        
-        // bring normal from tanspace into view space
-        viewNormal = normalize(mul(viewNormal, tanToView));
+        viewNormal = MapNormalViewSpace(normalize(viewTangent), normalize(viewBitangent), viewNormal, tc, normalMap, splr);
     }
     
 	// 顶点到点光源的向量
-    const float3 vectorToLight = lightPos - viewPos;
-    const float distVectorToLight = length(vectorToLight);
+    const float3 viewFragToLight = viewLightPos - viewPos;
+    const float distFragToLight = length(viewFragToLight);
 	// 顶点到点光源的方向
-    const float3 directionToLight = vectorToLight / distVectorToLight;
-	
-	// 漫反射衰减公式 diffuse attenuation
-    const float att = 1 / (attQuad * (distVectorToLight * distVectorToLight) + attLin * distVectorToLight + attConst);
-	// 漫反射颜色
-    const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(directionToLight, viewNormal));
-
-	// 镜面高光计算
-    const float3 w = viewNormal * dot(vectorToLight, viewNormal);
-    const float3 r = 2.0f * w - vectorToLight; // 反射光R的计算
-	// 镜面高光公式
-	//const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(worldPos))), specularPower);
+    const float3 directionToLight = viewFragToLight / distFragToLight;
 
     float3 specularReflectionColor;
     float specularPower = specularPowerConst;
@@ -81,8 +101,13 @@ float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 tangen
         specularReflectionColor = specularColor;
     }
     
-    const float3 specular = att * (diffuseColor * diffuseIntensity) * pow(max(0.0f, dot(normalize(-r), normalize(viewPos))), specularPower);
+    // 漫反射衰减公式 diffuse attenuation
+    const float att = Attenuate(attConst, attLin, attQuad, distFragToLight);
+	// 漫反射颜色
+    const float3 diffuse = Diffuse(diffuseColor, diffuseIntensity, att, directionToLight, viewNormal);
+    
+    const float3 specularReflected = Speculate(specularReflectionColor, 1.0f, viewNormal, viewFragToLight, viewPos, att, specularPower);
 
 	// 最终颜色 漫反射+环境光
-    return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specular * specularReflectionColor), 1.0f);
+    return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specularReflected), 1.0f);
 }
