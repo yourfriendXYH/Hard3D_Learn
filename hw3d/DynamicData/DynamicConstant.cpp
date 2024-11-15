9bb380169228c50cae0db473fbcbc4e6407827f9
VANKE\c-xuyh12 <c-xuyh12@vanke.com>
2024-11-15 14:59:54 +0800

    T41: 将DynamicConstant的宏和函数实现移至.cpp文件

---
+++ hw3d/DynamicData/DynamicConstant.cpp
@@ -0,0 +1,375 @@
+#include "DynamicConstant.h"
+
+#define DCB_RESOLVE_BASE(elementType) \
+size_t LayoutElement::Resolve ## elementType() const noexcept \
+{ \
+	assert(false); \
+	return 0; \
+}
+
+#define DCB_LEAF_ELEMENT_IMPL(elementType, sysType, hlslSize) \
+size_t elementType::Resolve ## elementType() const noexcept \
+{ \
+	return GetOffsetBegin(); \
+} \
+size_t elementType::GetOffsetEnd() const noexcept \
+{ \
+	return GetOffsetBegin() + ComputeSize(); \
+} \
+size_t elementType::Finalize(size_t offset) \
+{ \
+	m_offset = offset; \
+	return offset + ComputeSize(); \
+} \
+size_t elementType::ComputeSize() const noexcept \
+{ \
+	return hlslSize; \
+}
+
+#define DCB_LEAF_ELEMENT(elementType, sysType) DCB_LEAF_ELEMENT_IMPL(elementType, sysType, sizeof(sysType))
+
+#define DCB_REF_CONVERSION(refType,elementType,...) \
+refType::operator __VA_ARGS__ elementType::SystemType& () noexcept \
+{ \
+	return *reinterpret_cast<elementType::SystemType*>(m_pBytes + m_pLayout->Resolve ## elementType()); \
+}
+
+#define DCB_REF_ASSIGN(refType,elementType) \
+elementType::SystemType& refType::operator=(const elementType::SystemType& rhs) noexcept \
+{ \
+	return static_cast<elementType::SystemType&>(*this) = rhs; \
+}
+
+#define DCB_REF_NONCONST(refType,elementType) DCB_REF_CONVERSION(refType,elementType) DCB_REF_ASSIGN(refType,elementType)
+#define DCB_REF_CONST(refType,elementType) DCB_REF_CONVERSION(refType,elementType, const)
+
+#define DCB_PTR_CONVERSION(refType,elementType,...) \
+refType::operator __VA_ARGS__ elementType::SystemType*() noexcept \
+{ \
+	return &static_cast<__VA_ARGS__ elementType::SystemType&>(m_ref); \
+}
+
+namespace DynamicData
+{
+	LayoutElement::~LayoutElement()
+	{
+	}
+
+	LayoutElement& LayoutElement::operator[](const std::string& key)
+	{
+		assert(false);
+		return *this;
+	}
+
+	const LayoutElement& LayoutElement::operator[](const std::string& key) const
+	{
+		assert(false);
+		return *this;
+	}
+
+	LayoutElement& LayoutElement::LayoutEle()
+	{
+		assert(false);
+		return *this;
+	}
+
+	const LayoutElement& LayoutElement::LayoutEle() const
+	{
+		assert(false);
+		return *this;
+	}
+
+	size_t LayoutElement::GetOffsetBegin() const noexcept
+	{
+		return m_offset;
+	}
+
+	size_t LayoutElement::GetSizeInBytes() const noexcept
+	{
+		return GetOffsetEnd() - GetOffsetBegin();
+	}
+
+	size_t LayoutElement::GetNextBoundaryOffset(size_t offset)
+	{
+		return offset + (16u - offset % 16u) % 16u;
+	}
+
+	DCB_RESOLVE_BASE(Matrix);
+	DCB_RESOLVE_BASE(Float4);
+	DCB_RESOLVE_BASE(Float3);
+	DCB_RESOLVE_BASE(Float2);
+	DCB_RESOLVE_BASE(Float);
+	DCB_RESOLVE_BASE(Bool);
+
+	DCB_LEAF_ELEMENT(Matrix, DirectX::XMFLOAT4X4);
+	DCB_LEAF_ELEMENT(Float4, DirectX::XMFLOAT4);
+	DCB_LEAF_ELEMENT(Float3, DirectX::XMFLOAT3);
+	DCB_LEAF_ELEMENT(Float2, DirectX::XMFLOAT2);
+	DCB_LEAF_ELEMENT(Float, float);
+	DCB_LEAF_ELEMENT_IMPL(Bool, bool, 4u);
+
+	LayoutElement& Struct::operator[](const std::string& key)
+	{
+		return *m_map.at(key);
+	}
+
+	const LayoutElement& Struct::operator[](const std::string& key) const
+	{
+		return *m_map.at(key);
+	}
+
+	size_t Struct::GetOffsetEnd() const noexcept
+	{
+		// 上升到下一个边界（因为结构的大小是 16 的倍数）
+		return LayoutElement::GetNextBoundaryOffset(m_elements.back()->GetOffsetEnd());
+	}
+
+	void Struct::Add(const std::string& name, std::unique_ptr<LayoutElement> pElement) noexcept
+	{
+		m_elements.emplace_back(std::move(pElement));
+		if (!m_map.emplace(name, m_elements.back().get()).second)
+		{
+			assert(false);
+		}
+	}
+
+	size_t Struct::Finalize(size_t offset)
+	{
+		m_offset = offset;
+		size_t offsetNext = offset;
+		for (auto& pEle : m_elements)
+		{
+			offsetNext = (*pEle).Finalize(offsetNext); // 给结构体成员赋offset
+		}
+		return GetOffsetEnd();
+	}
+
+	size_t Struct::ComputeSize() const noexcept
+	{
+		// 通过对 大小 + 填充 求和来计算所有元素的偏移量
+		size_t offsetNext = 0u;
+		for (auto& ele : m_elements)
+		{
+			const auto elementSize = ele->ComputeSize(); // 结构体中单个元素的大小
+			offsetNext += CalculatePaddingBeforeElement(offsetNext, elementSize) + elementSize;
+
+		}
+		// 结构大小必须是 16 字节的倍数
+		return GetNextBoundaryOffset(offsetNext);
+	}
+
+	size_t Struct::CalculatePaddingBeforeElement(size_t offset, size_t size) noexcept
+	{
+		// 如果跨越 16 字节边界，则前进到下一个边界
+		if (offset / 16u != (offset + size - 1u) / 16u)
+		{
+			return GetNextBoundaryOffset(offset) - offset;
+		}
+		return 0u;
+		return offset; // 返回offset似乎是错的
+	}
+
+	size_t Array::GetOffsetEnd() const noexcept
+	{
+		assert(nullptr != m_upElement);
+		// 数组未在 hlsl 中打包
+		return GetOffsetBegin() + LayoutElement::GetNextBoundaryOffset(m_upElement->GetSizeInBytes()) * m_size;
+	}
+
+	void Array::Set(std::unique_ptr<LayoutElement> pElement, size_t size_in) noexcept
+	{
+		assert(nullptr == m_upElement);
+		m_upElement = std::move(pElement);
+		m_size = size_in;
+	}
+
+	LayoutElement& Array::LayoutEle()
+	{
+		return *m_upElement;
+	}
+
+	const LayoutElement& Array::LayoutEle() const
+	{
+		return *m_upElement;
+	}
+
+	size_t Array::Finalize(size_t offset)
+	{
+		assert(m_size != 0u && nullptr != m_upElement);
+		m_offset = offset;
+		m_upElement->Finalize(offset);
+		return GetOffsetEnd();
+	}
+
+	size_t Array::ComputeSize() const noexcept
+	{
+		// 数组未在 hlsl 中打包
+		return LayoutElement::GetNextBoundaryOffset(m_upElement->ComputeSize()) * m_size;
+	}
+
+	Layout::Layout()
+		:
+		m_pLayout(std::make_shared<Struct>())
+	{
+	}
+
+	Layout::Layout(std::shared_ptr<LayoutElement> pLayout)
+		:
+		m_pLayout(std::move(pLayout))
+	{
+	}
+
+	LayoutElement& Layout::operator[](const std::string& key)
+	{
+		return (*m_pLayout)[key];
+	}
+
+	size_t Layout::GetSizeInByte() const noexcept
+	{
+		return m_pLayout->GetSizeInBytes();
+	}
+
+	std::shared_ptr<LayoutElement> Layout::Finalize()
+	{
+		m_pLayout->Finalize(0); // 初始布局的偏移值
+		m_finalized = true;
+		return m_pLayout;
+	}
+
+	ConstElementRef::Ptr::Ptr(ConstElementRef& ref)
+		:
+		m_ref(ref)
+	{
+	}
+
+	DCB_PTR_CONVERSION(ConstElementRef::Ptr, Matrix, const);
+	DCB_PTR_CONVERSION(ConstElementRef::Ptr, Float4, const);
+	DCB_PTR_CONVERSION(ConstElementRef::Ptr, Float3, const);
+	DCB_PTR_CONVERSION(ConstElementRef::Ptr, Float2, const);
+	DCB_PTR_CONVERSION(ConstElementRef::Ptr, Float, const);
+	DCB_PTR_CONVERSION(ConstElementRef::Ptr, Bool, const);
+
+	ConstElementRef::ConstElementRef(const LayoutElement* pLayout, char* pBytes, size_t offset)
+		:
+		m_offset(offset),
+		m_pLayout(pLayout),
+		m_pBytes(pBytes)
+	{
+	}
+
+	ConstElementRef ConstElementRef::operator[](const std::string& key) noexcept
+	{
+		return { &(*m_pLayout)[key], m_pBytes, m_offset };
+	}
+
+	ConstElementRef ConstElementRef::operator[](size_t index) noexcept
+	{
+		// 数组未在 hlsl 中打包
+		const auto& t = m_pLayout->LayoutEle();
+		const auto elementSize = LayoutElement::GetNextBoundaryOffset(t.GetSizeInBytes());
+		return { &t, m_pBytes, m_offset + elementSize * index };
+	}
+
+	ConstElementRef::Ptr ConstElementRef::operator&() noexcept
+	{
+		return { *this };
+	}
+
+	DCB_REF_CONST(ConstElementRef, Matrix);
+	DCB_REF_CONST(ConstElementRef, Float4);
+	DCB_REF_CONST(ConstElementRef, Float3);
+	DCB_REF_CONST(ConstElementRef, Float2);
+	DCB_REF_CONST(ConstElementRef, Float);
+	DCB_REF_CONST(ConstElementRef, Bool);
+
+	ElementRef::Ptr::Ptr(ElementRef& ref)
+		:
+		m_ref(ref)
+	{
+	}
+
+	DCB_PTR_CONVERSION(ElementRef::Ptr, Matrix);
+	DCB_PTR_CONVERSION(ElementRef::Ptr, Float4);
+	DCB_PTR_CONVERSION(ElementRef::Ptr, Float3);
+	DCB_PTR_CONVERSION(ElementRef::Ptr, Float2);
+	DCB_PTR_CONVERSION(ElementRef::Ptr, Float);
+	DCB_PTR_CONVERSION(ElementRef::Ptr, Bool);
+
+	ElementRef::ElementRef(const LayoutElement* pLayout, char* pBytes, size_t offset)
+		:
+		m_pLayout(pLayout),
+		m_pBytes(pBytes),
+		m_offset(offset)
+	{
+	}
+
+	ElementRef::operator ConstElementRef() const noexcept
+	{
+		return { m_pLayout, m_pBytes, m_offset };
+	}
+
+	ElementRef ElementRef::operator[](const std::string& key) noexcept
+	{
+		return { &(*m_pLayout)[key], m_pBytes, m_offset };
+	}
+
+	ElementRef ElementRef::operator[](size_t index) noexcept
+	{
+		// 数组未在 hlsl 中打包
+		const auto& t = m_pLayout->LayoutEle();
+		const auto elementSize = LayoutElement::GetNextBoundaryOffset(t.GetSizeInBytes());
+		return { &t, m_pBytes, m_offset + elementSize * index };
+	}
+
+	ElementRef::Ptr ElementRef::operator&() noexcept
+	{
+		return { *this };
+	}
+
+	DCB_REF_NONCONST(ElementRef, Matrix);
+	DCB_REF_NONCONST(ElementRef, Float4);
+	DCB_REF_NONCONST(ElementRef, Float3);
+	DCB_REF_NONCONST(ElementRef, Float2);
+	DCB_REF_NONCONST(ElementRef, Float);
+	DCB_REF_NONCONST(ElementRef, Bool);
+
+
+
+	Buffer::Buffer(Layout& layout)
+		:
+		m_pLayout(static_pointer_cast<Struct>(layout.Finalize())),
+		m_bytes(m_pLayout->GetOffsetEnd()) // 根据布局初始化缓存大小
+	{
+	}
+
+	ElementRef Buffer::operator[](const std::string& key)
+	{
+		return { &(*m_pLayout)[key], m_bytes.data(), 0u };
+	}
+
+	ConstElementRef Buffer::operator[](const std::string& key) const noexcept
+	{
+		return const_cast<Buffer&>(*this)[key];
+	}
+
+	const char* Buffer::GetData() const noexcept
+	{
+		return m_bytes.data();
+	}
+
+	size_t Buffer::GetSizeInByte() const noexcept
+	{
+		return m_bytes.size();
+	}
+
+	const LayoutElement& Buffer::GetLayout() const noexcept
+	{
+		return *m_pLayout;
+	}
+
+	std::shared_ptr<LayoutElement> Buffer::CloneLayout() const
+	{
+		return m_pLayout;
+	}
+
+}
--- hw3d/DynamicData/DynamicConstant.h
+++ hw3d/DynamicData/DynamicConstant.h
@@ -3,63 +3,38 @@
 #include <unordered_map>
 #include <string>
 #include <memory>
+#include <DirectXMath.h>
 
-#define RESOLVE_BASE(elementType) \
-virtual size_t Resolve ## elementType() const noexcept \
-{ \
-	assert(false); \
-	return 0; \
-}
+#define DCB_RESOLVE_BASE(elementType) \
+virtual size_t Resolve ## elementType() const noexcept;
 
 // 布局元素宏
-#define LEAF_ELEMENT_IMPL(elementType, sysType, hlslSize) \
+#define DCB_LEAF_ELEMENT_IMPL(elementType, sysType, hlslSize) \
 class elementType : public LayoutElement \
 { \
 public: \
 	using SystemType = sysType; \
-	size_t Resolve ## elementType() const noexcept override final \
-	{ \
-		return GetOffsetBegin(); \
-	} \
-	size_t GetOffsetEnd() const noexcept override final \
-	{ \
-		return GetOffsetBegin() + ComputeSize(); \
-	} \
+	size_t Resolve ## elementType() const noexcept override final; \
+	size_t GetOffsetEnd() const noexcept override final; \
 protected: \
-	size_t Finalize(size_t offset) override final \
-	{ \
-		m_offset = offset; \
-		return offset + ComputeSize(); \
-	} \
-	size_t ComputeSize() const noexcept override final \
-	{ \
-		return hlslSize; \
-	} \
+	size_t Finalize(size_t offset) override final; \
+	size_t ComputeSize() const noexcept override final; \
 };
 
-#define LEAF_ELEMENT(elementType, sysType) LEAF_ELEMENT_IMPL(elementType, sysType, sizeof(sysType))
+#define DCB_LEAF_ELEMENT(elementType, sysType) DCB_LEAF_ELEMENT_IMPL(elementType, sysType, sizeof(sysType))
 
 // 类型转换运算符 和 赋值运算符 的重写(__VA_ARGS__多参数)
-#define REF_CONVERSION(elementType,...) \
-operator __VA_ARGS__ elementType::SystemType& () noexcept \
-{ \
-	return *reinterpret_cast<elementType::SystemType*>(m_pBytes + m_pLayout->Resolve ## elementType()); \
-} 
+#define DCB_REF_CONVERSION(elementType,...) \
+operator __VA_ARGS__ elementType::SystemType& () noexcept;
 
-#define REF_ASSIGN(elementType) \
-elementType::SystemType& operator=(const elementType::SystemType& rhs) noexcept \
-{ \
-	return static_cast<elementType::SystemType&>(*this) = rhs; \
-}
+#define DCB_REF_ASSIGN(elementType) \
+elementType::SystemType& operator=(const elementType::SystemType& rhs) noexcept;
 
-#define REF_NONCONST(elementType) REF_CONVERSION(elementType) REF_ASSIGN(elementType)
-#define REF_CONST(elementType) REF_CONVERSION(elementType, const)
+#define DCB_REF_NONCONST(elementType) DCB_REF_CONVERSION(elementType) DCB_REF_ASSIGN(elementType)
+#define DCB_REF_CONST(elementType) DCB_REF_CONVERSION(elementType, const)
 
-#define PTR_CONVERSION(elementType,...) \
-operator __VA_ARGS__ elementType::SystemType*() noexcept \
-{ \
-	return &static_cast<__VA_ARGS__ elementType::SystemType&>(m_ref); \
-}
+#define DCB_PTR_CONVERSION(elementType,...) \
+operator __VA_ARGS__ elementType::SystemType*() noexcept;
 
 namespace DynamicData
 {
@@ -73,46 +48,25 @@ namespace DynamicData
 		friend class Array;
 		friend class Layout;
 	public:
-		virtual ~LayoutElement()
-		{
-		}
+		virtual ~LayoutElement();
 
 		// [] 仅适用于结构体；通过名称访问成员
-		virtual LayoutElement& operator[](const std::string& key)
-		{
-			assert(false);
-			return *this;
-		}
-		virtual const LayoutElement& operator[](const std::string& key) const
-		{
-			assert(false);
-			return *this;
-		}
+		virtual LayoutElement& operator[](const std::string& key);
+
+		virtual const LayoutElement& operator[](const std::string& key) const;
 
 		// LayoutEle() 仅适用于数组；获取数组类型布局对象
-		virtual LayoutElement& LayoutEle()
-		{
-			assert(false);
-			return *this;
-		}
-		virtual const LayoutElement& LayoutEle() const
-		{
-			assert(false);
-			return *this;
-		}
+		virtual LayoutElement& LayoutEle();
+
+		virtual const LayoutElement& LayoutEle() const;
 
 		// 基于偏移量的函数 仅在完成后才起作用！
-		size_t GetOffsetBegin() const noexcept
-		{
-			return m_offset;
-		}
+		size_t GetOffsetBegin() const noexcept;
+
 		virtual size_t GetOffsetEnd() const noexcept = 0;
 
 		// 获取从偏移量得出的字节大小
-		size_t GetSizeInBytes() const noexcept
-		{
-			return GetOffsetEnd() - GetOffsetBegin();
-		}
+		size_t GetSizeInBytes() const noexcept;
 
 		// 仅适用于 Structs；添加 LayoutElement
 		template<typename T>
@@ -123,17 +77,14 @@ namespace DynamicData
 		LayoutElement& Set(size_t size_in) noexcept;
 
 		// 返回偏移量的值，该值上升到下一个 16 字节边界（如果尚未到达边界）
-		static size_t GetNextBoundaryOffset(size_t offset)
-		{
-			return offset + (16u - offset % 16u) % 16u;
-		}
+		static size_t GetNextBoundaryOffset(size_t offset);
 
-		RESOLVE_BASE(Matrix);
-		RESOLVE_BASE(Float4);
-		RESOLVE_BASE(Float3);
-		RESOLVE_BASE(Float2);
-		RESOLVE_BASE(Float);
-		RESOLVE_BASE(Bool);
+		DCB_RESOLVE_BASE(Matrix);
+		DCB_RESOLVE_BASE(Float4);
+		DCB_RESOLVE_BASE(Float3);
+		DCB_RESOLVE_BASE(Float2);
+		DCB_RESOLVE_BASE(Float);
+		DCB_RESOLVE_BASE(Bool);
 
 	protected:
 		// 设置元素和子元素的所有偏移量，返回此元素之后的偏移量
@@ -145,81 +96,34 @@ namespace DynamicData
 		size_t m_offset = 0u;
 	};
 
-	LEAF_ELEMENT(Matrix, DirectX::XMFLOAT4X4);
-	LEAF_ELEMENT(Float4, DirectX::XMFLOAT4);
-	LEAF_ELEMENT(Float3, DirectX::XMFLOAT3);
-	LEAF_ELEMENT(Float2, DirectX::XMFLOAT2);
-	LEAF_ELEMENT(Float, float);
-	LEAF_ELEMENT_IMPL(Bool, bool, 4u);
+	DCB_LEAF_ELEMENT(Matrix, DirectX::XMFLOAT4X4);
+	DCB_LEAF_ELEMENT(Float4, DirectX::XMFLOAT4);
+	DCB_LEAF_ELEMENT(Float3, DirectX::XMFLOAT3);
+	DCB_LEAF_ELEMENT(Float2, DirectX::XMFLOAT2);
+	DCB_LEAF_ELEMENT(Float, float);
+	DCB_LEAF_ELEMENT_IMPL(Bool, bool, 4u);
 
 	// 结构体布局
 	class Struct : public LayoutElement
 	{
 	public:
 		//using LayoutElement::LayoutElement; // 使用基类的构造函数
-		LayoutElement& operator[](const std::string& key) override final
-		{
-			return *m_map.at(key);
-		}
+		LayoutElement& operator[](const std::string& key) override final;
 
-		const LayoutElement& operator[](const std::string& key) const override final
-		{
-			return *m_map.at(key);
-		}
+		const LayoutElement& operator[](const std::string& key) const override final;
 
-		size_t GetOffsetEnd() const noexcept override final
-		{
-			// 上升到下一个边界（因为结构的大小是 16 的倍数）
-			return LayoutElement::GetNextBoundaryOffset(m_elements.back()->GetOffsetEnd());
-		}
+		size_t GetOffsetEnd() const noexcept override final;
 
 		// 添加元素布局
-		void Add(const std::string& name, std::unique_ptr<LayoutElement> pElement) noexcept
-		{
-			m_elements.emplace_back(std::move(pElement));
-			if (!m_map.emplace(name, m_elements.back().get()).second)
-			{
-				assert(false);
-			}
-		}
+		void Add(const std::string& name, std::unique_ptr<LayoutElement> pElement) noexcept;
 
 	protected:
-		size_t Finalize(size_t offset) override final
-		{
-			m_offset = offset;
-			size_t offsetNext = offset;
-			for (auto& pEle : m_elements)
-			{
-				offsetNext = (*pEle).Finalize(offsetNext); // 给结构体成员赋offset
-			}
-			return GetOffsetEnd();
-		}
+		size_t Finalize(size_t offset) override final;
 
-		size_t ComputeSize() const noexcept override final
-		{
-			// 通过对 大小 + 填充 求和来计算所有元素的偏移量
-			size_t offsetNext = 0u;
-			for (auto& ele : m_elements)
-			{
-				const auto elementSize = ele->ComputeSize(); // 结构体中单个元素的大小
-				offsetNext += CalculatePaddingBeforeElement(offsetNext, elementSize) + elementSize;
-
-			}
-			// 结构大小必须是 16 字节的倍数
-			return GetNextBoundaryOffset(offsetNext);
-		}
+		size_t ComputeSize() const noexcept override final;
 
 	private:
-		static size_t CalculatePaddingBeforeElement(size_t offset, size_t size) noexcept
-		{
-			// 如果跨越 16 字节边界，则前进到下一个边界
-			if (offset / 16u != (offset + size - 1u) / 16u)
-			{
-				return GetNextBoundaryOffset(offset) - offset;
-			}
-			return 0u;
-			return offset; // 返回offset似乎是错的
-		}
+		static size_t CalculatePaddingBeforeElement(size_t offset, size_t size) noexcept;
 
 	private:
 		std::unordered_map<std::string, LayoutElement*> m_map; // 成员名对应的布局
@@ -229,44 +133,20 @@ namespace DynamicData
 	class Array : public LayoutElement
 	{
 	public:
-		size_t GetOffsetEnd() const noexcept override final
-		{
-			assert(nullptr != m_upElement);
-			// 数组未在 hlsl 中打包
-			return GetOffsetBegin() + LayoutElement::GetNextBoundaryOffset(m_upElement->GetSizeInBytes()) * m_size;
-		}
+		size_t GetOffsetEnd() const noexcept override final;
 
-		void Set(std::unique_ptr<LayoutElement> pElement, size_t size_in) noexcept
-		{
-			assert(nullptr == m_upElement);
-			m_upElement = std::move(pElement);
-			m_size = size_in;
-		}
+		void Set(std::unique_ptr<LayoutElement> pElement, size_t size_in) noexcept;
 
 		// 拿到数组的元素布局
-		LayoutElement& LayoutEle() override final
-		{
-			return *m_upElement;
-		}
-		const LayoutElement& LayoutEle() const override final
-		{
-			return *m_upElement;
-		}
+		LayoutElement& LayoutEle() override final;
+
+		const LayoutElement& LayoutEle() const override final;
+
 	protected:
 		// 数组末端的offset
-		size_t Finalize(size_t offset) override final
-		{
-			assert(m_size != 0u && nullptr != m_upElement);
-			m_offset = offset;
-			m_upElement->Finalize(offset);
-			return GetOffsetEnd();
-		}
+		size_t Finalize(size_t offset) override final;
 
-		size_t ComputeSize() const noexcept override final
-		{
-			// 数组未在 hlsl 中打包
-			return LayoutElement::GetNextBoundaryOffset(m_upElement->ComputeSize()) * m_size;
-		}
+		size_t ComputeSize() const noexcept override final;
 
 	private:
 		size_t m_size = 0u; // 数组大小
@@ -295,27 +175,13 @@ namespace DynamicData
 	class Layout
 	{
 	public:
-		Layout()
-			:
-			m_pLayout(std::make_shared<Struct>())
-		{
-		}
+		Layout();
 
-		Layout(std::shared_ptr<LayoutElement> pLayout)
-			:
-			m_pLayout(std::move(pLayout))
-		{
-		}
+		Layout(std::shared_ptr<LayoutElement> pLayout);
 
-		LayoutElement& operator[](const std::string& key)
-		{
-			return (*m_pLayout)[key];
-		}
+		LayoutElement& operator[](const std::string& key);
 
-		size_t GetSizeInByte() const noexcept
-		{
-			return m_pLayout->GetSizeInBytes();
-		}
+		size_t GetSizeInByte() const noexcept;
 
 		template<typename T>
 		LayoutElement& Add(const std::string& key) noexcept
@@ -323,12 +189,7 @@ namespace DynamicData
 			return m_pLayout->Add<T>(key);
 		}
 
-		std::shared_ptr<LayoutElement> Finalize()
-		{
-			m_pLayout->Finalize(0); // 初始布局的偏移值
-			m_finalized = true;
-			return m_pLayout;
-		}
+		std::shared_ptr<LayoutElement> Finalize();
 
 	private:
 		bool m_finalized = false;
@@ -341,55 +202,33 @@ namespace DynamicData
 		class Ptr
 		{
 		public:
-			Ptr(ConstElementRef& ref)
-				:
-				m_ref(ref)
-			{
-			}
+			Ptr(ConstElementRef& ref);
 
-			PTR_CONVERSION(Matrix, const);
-			PTR_CONVERSION(Float4, const);
-			PTR_CONVERSION(Float3, const);
-			PTR_CONVERSION(Float2, const);
-			PTR_CONVERSION(Float, const);
-			PTR_CONVERSION(Bool, const);
+			DCB_PTR_CONVERSION(Matrix, const);
+			DCB_PTR_CONVERSION(Float4, const);
+			DCB_PTR_CONVERSION(Float3, const);
+			DCB_PTR_CONVERSION(Float2, const);
+			DCB_PTR_CONVERSION(Float, const);
+			DCB_PTR_CONVERSION(Bool, const);
 
 		private:
 			ConstElementRef& m_ref;
 		};
 	public:
-		ConstElementRef(const LayoutElement* pLayout, char* pBytes, size_t offset)
-			:
-			m_offset(offset),
-			m_pLayout(pLayout),
-			m_pBytes(pBytes)
-		{
-		}
+		ConstElementRef(const LayoutElement* pLayout, char* pBytes, size_t offset);
 
-		ConstElementRef operator[](const std::string& key) noexcept
-		{
-			return { &(*m_pLayout)[key], m_pBytes, m_offset };
-		}
+		ConstElementRef operator[](const std::string& key) noexcept;
 
-		ConstElementRef operator[](size_t index) noexcept
-		{
-			// 数组未在 hlsl 中打包
-			const auto& t = m_pLayout->LayoutEle();
-			const auto elementSize = LayoutElement::GetNextBoundaryOffset(t.GetSizeInBytes());
-			return { &t, m_pBytes, m_offset + elementSize * index };
-		}
+		ConstElementRef operator[](size_t index) noexcept;
 
-		Ptr operator&() noexcept
-		{
-			return { *this };
-		}
+		Ptr operator&() noexcept;
 
-		REF_CONST(Matrix);
-		REF_CONST(Float4);
-		REF_CONST(Float3);
-		REF_CONST(Float2);
-		REF_CONST(Float);
-		REF_CONST(Bool);
+		DCB_REF_CONST(Matrix);
+		DCB_REF_CONST(Float4);
+		DCB_REF_CONST(Float3);
+		DCB_REF_CONST(Float2);
+		DCB_REF_CONST(Float);
+		DCB_REF_CONST(Bool);
 
 	private:
 		size_t m_offset;
@@ -403,61 +242,36 @@ namespace DynamicData
 		class Ptr
 		{
 		public:
-			Ptr(ElementRef& ref)
-				:
-				m_ref(ref)
-			{
-			}
+			Ptr(ElementRef& ref);
 
-			PTR_CONVERSION(Matrix);
-			PTR_CONVERSION(Float4);
-			PTR_CONVERSION(Float3);
-			PTR_CONVERSION(Float2);
-			PTR_CONVERSION(Float);
-			PTR_CONVERSION(Bool);
+			DCB_PTR_CONVERSION(Matrix);
+			DCB_PTR_CONVERSION(Float4);
+			DCB_PTR_CONVERSION(Float3);
+			DCB_PTR_CONVERSION(Float2);
+			DCB_PTR_CONVERSION(Float);
+			DCB_PTR_CONVERSION(Bool);
 
 		private:
 			ElementRef& m_ref;
 		};
 
 	public:
-		ElementRef(const LayoutElement* pLayout, char* pBytes, size_t offset)
-			:
-			m_pLayout(pLayout),
-			m_pBytes(pBytes),
-			m_offset(offset)
-		{
-		}
+		ElementRef(const LayoutElement* pLayout, char* pBytes, size_t offset);
 
-		operator ConstElementRef() const noexcept
-		{
-			return { m_pLayout, m_pBytes, m_offset };
-		}
+		operator ConstElementRef() const noexcept;
 
-		ElementRef operator[](const std::string& key) noexcept
-		{
-			return { &(*m_pLayout)[key], m_pBytes, m_offset };
-		}
+		ElementRef operator[](const std::string& key) noexcept;
 
-		ElementRef operator[](size_t index) noexcept
-		{
-			// 数组未在 hlsl 中打包
-			const auto& t = m_pLayout->LayoutEle();
-			const auto elementSize = LayoutElement::GetNextBoundaryOffset(t.GetSizeInBytes());
-			return { &t, m_pBytes, m_offset + elementSize * index };
-		}
+		ElementRef operator[](size_t index) noexcept;
 
-		Ptr operator&() noexcept
-		{
-			return { *this };
-		}
+		Ptr operator&() noexcept;
 
-		REF_NONCONST(Matrix);
-		REF_NONCONST(Float4);
-		REF_NONCONST(Float3);
-		REF_NONCONST(Float2);
-		REF_NONCONST(Float);
-		REF_NONCONST(Bool);
+		DCB_REF_NONCONST(Matrix);
+		DCB_REF_NONCONST(Float4);
+		DCB_REF_NONCONST(Float3);
+		DCB_REF_NONCONST(Float2);
+		DCB_REF_NONCONST(Float);
+		DCB_REF_NONCONST(Bool);
 
 	private:
 		const class LayoutElement* m_pLayout;
@@ -468,41 +282,19 @@ namespace DynamicData
 	class Buffer
 	{
 	public:
-		Buffer(Layout& layout)
-			:
-			m_pLayout(static_pointer_cast<Struct>(layout.Finalize())),
-			m_bytes(m_pLayout->GetOffsetEnd()) // 根据布局初始化缓存大小
-		{
-		}
-		ElementRef operator[](const std::string& key)
-		{
-			return { &(*m_pLayout)[key], m_bytes.data(), 0u };
-		}
+		Buffer(Layout& layout);
 
-		ConstElementRef operator[](const std::string& key) const noexcept
-		{
-			return const_cast<Buffer&>(*this)[key];
-		}
+		ElementRef operator[](const std::string& key);
 
-		const char* GetData() const noexcept
-		{
-			return m_bytes.data();
-		}
+		ConstElementRef operator[](const std::string& key) const noexcept;
 
-		size_t GetSizeInByte() const noexcept
-		{
-			return m_bytes.size();
-		}
+		const char* GetData() const noexcept;
 
-		const LayoutElement& GetLayout() const noexcept
-		{
-			return *m_pLayout;
-		}
+		size_t GetSizeInByte() const noexcept;
 
-		std::shared_ptr<LayoutElement> CloneLayout() const
-		{
-			return m_pLayout;
-		}
+		const LayoutElement& GetLayout() const noexcept;
+
+		std::shared_ptr<LayoutElement> CloneLayout() const;
 
 	private:
 		std::shared_ptr<Struct> m_pLayout; // 常数缓存的布局
